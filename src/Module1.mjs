e933c223f41cb61cb050b05dc2cb9ee2138a6ca2
VIVEK KUMAR SONI <119720668+vivekkumarsoni123@users.noreply.github.com>
2024-08-21 13:31:22 +0530

    Add files via upload

---
+++ src/Module1.mjs
@@ -0,0 +1,6 @@
+import ui, {b,c,d} from './Module2.mjs';
+
+console.log(ui);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ src/Module2.mjs
@@ -0,0 +1,9 @@
+const a = 'vivek';
+const b = 'Kumar';
+const c = 'Soni';
+const d = 'Ranchi';
+
+export default a;
+export {b};
+export {c};
+export {d};
