31268754896b8a950b810441c9b5fb7db1147445
VIVEK KUMAR SONI <119720668+vivekkumarsoni123@users.noreply.github.com>
2024-08-21 21:48:22 +0530

    Delete src directory

--- src/Module1.mjs
+++
@@ -1,6 +0,0 @@
-import ui, {b,c,d} from './Module2.mjs';
-
-console.log(ui);
-console.log(b);
-console.log(c);
-console.log(d);
--- src/Module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = 'vivek';
-const b = 'Kumar';
-const c = 'Soni';
-const d = 'Ranchi';
-
-export default a;
-export {b};
-export {c};
-export {d};
